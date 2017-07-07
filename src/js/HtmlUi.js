18c92ca5ca10c81b22362ede9186e5e2b6c25c41
Ivo Toby <i.toby@wearetriple.com>
2017-07-07 15:14:18 +0200

    removed loading template from remote add hide/show-controls event-handlers

--- src/js/HtmlUi.js
+++ src/js/HtmlUi.js
@@ -5,7 +5,7 @@ import { extractDirectiveNodes, createAttachDirective } from './lib/directive-no
 import packageJson from '../../package.json';
 import defaultTheme from './defaultTheme';
 
-let TIME_TILL_FADE = 3000;
+
 /**
  * Plugin that can be used to supply your own HTMLElements that make up the ui. By specifying
  * data-attributes you can customize the functionality of the ui.
@@ -20,11 +20,7 @@ class HtmlUi extends Meister.Ui {
      */
     constructor(config, meister) {
         super(config, meister);
-
-        if (Number.isFinite(this.config.timeToFade)) {
-            TIME_TILL_FADE = this.config.timeToFade * 1000;
-        }
-
+        this.hiddenClassName = this.config.hiddenClassName || 'mstr-hide-controls';
         if (!this.config.ui) {
             // use default, also triggered if the UI element is defined but not found
             this.insertStringTemplate(defaultTheme());
@@ -33,14 +29,6 @@ class HtmlUi extends Meister.Ui {
         if (this.meister.utils.isDOMNode(this.config.ui)) {
             this.element = this.config.ui;
             this.processTemplate();
-        } else if (this.config.ui.indexOf('http://') > -1 ||
-            this.config.ui.indexOf('https://') > -1) {
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', this.config.ui);
-            xhr.responseType = 'document';
-            xhr.addEventListener('load', this.onTemplateRequestSuccess.bind(this), false);
-            xhr.addEventListener('error', this.onTemplateRequestError.bind(this), false);
-            xhr.send();
         } else if (typeof this.config.ui === 'string') {
             // insert string template configured by user
             this.insertStringTemplate(this.config.ui);
@@ -94,38 +82,19 @@ class HtmlUi extends Meister.Ui {
     }
 
     /**
-     * Check the response status, and on a succes extract the first node to use as the custom ui.
-     * @param {ProgressEvent} loadEvent Event object from the XMLHttpRequest.
-     */
-    onTemplateRequestSuccess(loadEvent) {
-        if (loadEvent.target.status < 200 || loadEvent.target.status >= 400) {
-            this.onTemplateRequestError(loadEvent);
-            return;
-        }
-
-        this.element = loadEvent.target.response.activeElement.firstChild;
-        this.processTemplate();
-        // Force a redraw of the ui.
-        this.draw();
-        // Notify user that the remote template has been parsed and drawn successfully.
-        if (this.config.remoteTemplateReady) { this.config.remoteTemplateReady(); }
-    }
-
-    /**
-     * Log the XMLHttpRequest to the console.
-     * @param {ProgressEvent} loadEvent Event object from the XMLHttpRequest.
-     */
-    onTemplateRequestError(loadEvent) {
-        console.error(`${HtmlUi.pluginName}: error loading the template. `, loadEvent.target);
-    }
-
-    /**
      * Replace marked nodes with their StandardUi equivalents, and register events on the specified nodes.
      */
     processTemplate() {
         extractStandardNodes(this.element).forEach(createLoadStandardElement(this.meister, this.config.standard));
         extractEventNodes(this.element).forEach(createRegisterDataEvents(this.meister, this.config.registeredCallback));
-        extractDirectiveNodes(this.element).forEach(createAttachDirective(this.meister, this.config.registeredCallback));
+        extractDirectiveNodes(this.element).forEach(createAttachDirective(this.meister, this.config));
+
+        // set events
+        this.meister.on('uiEvent:hideControls', this.hideControls.bind(this));
+        this.meister.on('uiEvent:showControls', this.showControls.bind(this));
+        this.meister.on('uiEvent:hideCursor', this.hideCursor.bind(this));
+        this.meister.on('uiEvent:showCursor', this.showCursor.bind(this));
+
     }
 
     /**
@@ -137,66 +106,22 @@ class HtmlUi extends Meister.Ui {
         this.controlsWrapper.appendChild(this.element);
     }
 
-    onMouseDown() {
-        this.dragging = true;
-        window.addEventListener('mouseup', this.onMouseUp);
-        window.addEventListener('touchend', this.onMouseUp);
-    }
-
-    onMouseUp() {
-        this.dragging = false;
-        // Fake mouse movement
-        this.onMouseMove();
-
-        window.removeEventListener('mouseup', this.onMouseUp);
-        window.removeEventListener('touchend', this.onMouseUp);
-    }
-
-    onMouseMove() {
-        clearTimeout(this.mouseTimeout);
-        clearTimeout(this.controlsTimeout);
-
-        this.showControls();
-        this.showCursor();
-
-        this.mouseTimeout = setTimeout(this.hideCursor.bind(this), TIME_TILL_FADE);
-        this.controlsTimeout = setTimeout(this.hideControls.bind(this), TIME_TILL_FADE);
-    }
-
-    onMouseLeave() {
-        clearTimeout(this.mouseTimeout);
-        clearTimeout(this.controlsTimeout);
-
-        this.hideControls();
-        this.showCursor();
-    }
-
     hideControls() {
-        if (this.dragging || !this.meister.playing) {
-            return;
-        }
-
-        this.bottomBar.getNode().classList.add('hidden');
-        if (!this.config.condensedUi) { this.topBar.getNode().classList.add('hidden'); }
-        this.bottomBar.hide();
-
-        if (this.isControlsShown) {
-            this.meister.trigger('uiEvent:hideControls', {});
-        }
-
-        this.isControlsShown = false;
+        if (this.element) this.element.classList.add(this.hiddenClassName);
     }
 
     showControls() {
-        this.bottomBar.getNode().classList.remove('hidden');
-        if (!this.config.condensedUi) { this.topBar.getNode().classList.remove('hidden'); }
-
-        if (!this.isControlsShown) {
-            this.meister.trigger('uiEvent:showControls', {});
-        }
-
-        this.isControlsShown = true;
+        if (this.element) this.element.classList.remove(this.hiddenClassName);
     }
+
+    hideCursor() {
+        this.meister.container.style.cursor = 'none';
+    }
+
+    showCursor() {
+        this.meister.container.style.cursor = 'auto';
+    }
+
 
 }
 
