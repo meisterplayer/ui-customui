055f3d053578c23ca3959dc3bcf836cb89eae526
Franklin Waller <fr.l.waller@gmail.com>
2018-02-06 16:11:10 +0100

    feat(directives): Add TogglePlayPauseClick directive

--- src/js/directives/createDirective.js
+++ src/js/directives/createDirective.js
@@ -1,9 +1,12 @@
 import AutoHideShow from './AutoHideShow';
+import TogglePlayPauseClick from './TogglePlayPauseClick';
 
 export default function createDirective(meister, config, name, element) {
     switch (name) {
     case 'autohideshow':
         return AutoHideShow(meister, config, element);
+    case 'toggleplaypauseclick':
+        return new TogglePlayPauseClick(meister, config, element);
     default:
         return null;
     }
