c95cfd0e25d0087f55b656af987c9d834670c7d1
Thomas van Dam <t.vandam@wearetriple.com>
2018-02-19 14:19:30 +0100

    fix(click): Ignore clicks on child elements

--- src/js/directives/TogglePlayPauseClick.js
+++ src/js/directives/TogglePlayPauseClick.js
@@ -9,14 +9,17 @@ class TogglePlayPauseClick extends ProtoDirective {
         // Taken from StandardUi
         this.meister.config.playPauseOnScreenClick = this.meister.config.playPauseOnScreenClick === undefined ? true : this.meister.config.playPauseOnScreenClick;
 
-        element.addEventListener('click', this.onElementClick.bind(this));
+        this.onElementClick = this.onElementClick.bind(this);
+        this.element = element;
+
+        this.element.addEventListener('click', this.onElementClick);
 
         this.meister.on('itemTimeInfo', (timeInfo) => {
             this.isLive = timeInfo.isLive;
         });
     }
 
-    onElementClick() {
+    onElementClick(e) {
         // Don't use this directive on mobile devices since these are annoying for users.
         if (this.meister.browser.isMobile) {
             return;
@@ -26,11 +29,20 @@ class TogglePlayPauseClick extends ProtoDirective {
             return;
         }
 
+        // Ignore clicks on child elements.
+        if (e.target !== this.element) {
+            return;
+        }
+
         if (this.meister.playing) {
             this.meister.pause(true);
         } else {
             this.meister.play(true);
         }
+    }
+
+    unload() {
+        this.element.removeEventListener(this.onElementClick);
     }
 }
 
